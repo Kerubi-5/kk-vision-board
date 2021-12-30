d4e2a490fa180c3215e7881e239619b2275e3b4d
John Kim Querobines <querobinesj@gmail.com>
2021-12-30 12:20:36 +0800

    fixes envelope

--- src/components/Envelope.tsx
+++ src/components/Envelope.tsx
@@ -1,10 +1,17 @@
 import { useState } from "react";
-const Envelope = () => {
+interface IProps {
+  text: {
+    message1: string;
+    message2: string;
+  };
+}
+const Envelope: React.FC<IProps> = ({ text }) => {
   const [isOpen, setOpen] = useState(false);
 
   const toggleEnvelope = () => {
     setOpen(!isOpen);
   };
+
   return (
     <div>
       <div
@@ -13,7 +20,10 @@ const Envelope = () => {
       >
         <div className="front flap"></div>
         <div className="front pocket"></div>
-        <div className="letter"></div>
+        <div className="letter">
+          <div>{text.message1}</div>
+          <div className="words">{text.message2}</div>
+        </div>
         <div className="hearts">
           <div className="heart a1"></div>
           <div className="heart a2"></div>
--- src/containers/Goals.tsx
+++ src/containers/Goals.tsx
@@ -1,16 +1,44 @@
+import { useState } from "react";
 import Envelope from "../components/Envelope";
 
+interface IState {
+  text: {
+    message1: string;
+    message2: string;
+  }[];
+}
+
 const Goals = () => {
+  const myText = [
+    {
+      message1: "My goal",
+      message2: "Message 2sdds",
+    },
+    {
+      message1: "Another goal",
+      message2: "Message ddsasd2",
+    },
+    {
+      message1: "And another goal",
+      message2: "Message 222222",
+    },
+    {
+      message1: "Last goal",
+      message2: "Message 3334442",
+    },
+  ];
+  const [messages, setMessage] = useState<IState["text"]>(myText);
+
+  const renderGoalList = (): JSX.Element[] => {
+    return messages.map((data) => {
+      return <Envelope text={data} />;
+    });
+  };
   return (
     <>
       <div className="section">
         <div className="section__title">GOALS</div>
-        <div className="envelope-wrapper">
-          <Envelope />
-          <Envelope />
-          <Envelope />
-          <Envelope />
-        </div>
+        <div className="envelope-wrapper">{renderGoalList()}</div>
       </div>
     </>
   );
