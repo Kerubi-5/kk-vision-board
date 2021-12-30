0da4490c32a5eac4660d292dcc5673a839b27572
John Kim Querobines <querobinesj@gmail.com>
2021-12-30 14:17:17 +0800

    fixes gallery

--- src/containers/Museo.tsx
+++ src/containers/Museo.tsx
@@ -1,125 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
+import classNames from "classnames";
+
+interface IState {
+  imgState: {
+    imgSrc: string;
+    active: boolean;
+  };
+}
 
 const Museo = () => {
+  const [openImg, setOpenImg] = useState<IState["imgState"]>({
+    imgSrc: "",
+    active: false,
+  });
+  const data = [
+    {
+      imgSrc: "https://picsum.photos/300/300",
+    },
+    {
+      imgSrc: "https://picsum.photos/300/300",
+    },
+    {
+      imgSrc: "https://picsum.photos/300/300",
+    },
+    {
+      imgSrc: "https://picsum.photos/300/300",
+    },
+    {
+      imgSrc: "https://picsum.photos/300/400",
+    },
+    {
+      imgSrc: "https://picsum.photos/300/400",
+    },
+  ];
+
+  const renderImage = (): JSX.Element[] => {
+    return data.map((data) => {
+      return (
+        <div className="-fx-gal-item" onClick={() => handleClick(data.imgSrc)}>
+          <div className="-fx-gal-image-thumb">
+            <img src={data.imgSrc} />
+          </div>
+        </div>
+      );
+    });
+  };
+
+  const handleClick = (img: string) => {
+    setOpenImg({ imgSrc: img, active: !openImg.active });
+  };
   return (
     <>
       <div className="section">
         <div className="section__title">Zaky</div>
-        <div className="-fx-image-gal">
-          <div className="-fx-gal-item">
-            <div className="-fx-gal-image-thumb">
-              <a href="https://codepen.io/aledebarba" target="_blank">
-                <img src="https://picsum.photos/id/237/300/300" />
-              </a>
-            </div>
-          </div>
-
-          <div className="-fx-gal-item">
-            <div className="-fx-gal-image-thumb">
-              <a href="https://codepen.io/aledebarba" target="_blank">
-                <img src="https://picsum.photos/id/238/300/300" />
-              </a>
-            </div>
-          </div>
-
-          <div className="-fx-gal-item">
-            <div className="-fx-gal-image-thumb">
-              <a href="https://codepen.io/aledebarba" target="_blank">
-                <img src="https://picsum.photos/id/39/300/300" />
-              </a>
-            </div>
-          </div>
-
-          <div className="-fx-gal-item">
-            <div className="-fx-gal-image-thumb">
-              <a href="https://codepen.io/aledebarba" target="_blank">
-                <img src="https://picsum.photos/id/240/300/300" />
-              </a>
-            </div>
-          </div>
-
-          <div className="-fx-gal-item">
-            <div className="-fx-gal-image-thumb">
-              <a href="https://codepen.io/aledebarba" target="_blank">
-                <img src="https://picsum.photos/id/341/300/300" />
-              </a>
-            </div>
-          </div>
-
-          <div className="-fx-gal-item">
-            <div className="-fx-gal-image-thumb">
-              <a href="https://codepen.io/aledebarba" target="_blank">
-                <img src="https://picsum.photos/id/142/300/300" />
-              </a>
-            </div>
-          </div>
-        </div>
+        <div className="-fx-image-gal">{renderImage()}</div>
       </div>
-      <div className="section">
-        <div className="section__title">Islet</div>
-        <div className="-fx-image-gal">
-          <div className="-fx-gal-item">
-            <div className="-fx-gal-image-thumb">
-              <a href="https://codepen.io/aledebarba" target="_blank">
-                <img src="https://picsum.photos/id/237/300/300" />
-              </a>
-            </div>
-          </div>
 
-          <div className="-fx-gal-item">
-            <div className="-fx-gal-image-thumb">
-              <a href="https://codepen.io/aledebarba" target="_blank">
-                <img src="https://picsum.photos/id/238/300/300" />
-              </a>
-            </div>
-          </div>
-
-          <div className="-fx-gal-item">
-            <div className="-fx-gal-image-thumb">
-              <a href="https://codepen.io/aledebarba" target="_blank">
-                <img src="https://picsum.photos/id/39/300/300" />
-              </a>
-            </div>
-          </div>
-
-          <div className="-fx-gal-item">
-            <div className="-fx-gal-image-thumb">
-              <a href="https://codepen.io/aledebarba" target="_blank">
-                <img src="https://picsum.photos/id/240/300/300" />
-              </a>
-            </div>
-          </div>
-
-          <div className="-fx-gal-item">
-            <div className="-fx-gal-image-thumb">
-              <a href="https://codepen.io/aledebarba" target="_blank">
-                <img src="https://picsum.photos/id/341/300/300" />
-              </a>
-            </div>
-          </div>
-
-          <div className="-fx-gal-item">
-            <div className="-fx-gal-image-thumb">
-              <a href="https://codepen.io/aledebarba" target="_blank">
-                <img src="https://picsum.photos/id/142/300/300" />
-              </a>
-            </div>
-          </div>
-          <div className="-fx-gal-item">
-            <div className="-fx-gal-image-thumb">
-              <a href="https://codepen.io/aledebarba" target="_blank">
-                <img src="https://picsum.photos/id/142/300/300" />
-              </a>
-            </div>
-          </div>
-          <div className="-fx-gal-item">
-            <div className="-fx-gal-image-thumb">
-              <a href="https://codepen.io/aledebarba" target="_blank">
-                <img src="https://picsum.photos/id/142/300/300" />
-              </a>
-            </div>
-          </div>
-        </div>
+      <div
+        className={classNames("modal", { active: openImg.active })}
+        onClick={() => handleClick("")}
+      >
+        <span className="close">&times;</span>
+        <img className="modal-content" src={openImg.imgSrc} />
+        <div id="caption">KK</div>
       </div>
     </>
   );
